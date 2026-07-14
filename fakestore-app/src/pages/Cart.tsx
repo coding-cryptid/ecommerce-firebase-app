828138985617f5ceadf8f732d7e7634343891b23
Gianna Cosenza <giacosey28@gmail.com>
2026-07-14 10:39:47 -0400

    Added empty cart placeholder.

---
+++ fakestore-app/src/pages/Cart.tsx
@@ -0,0 +1,10 @@
+const Cart = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Shopping Cart</h1>
+      <p>Cart contents will go here soon!</p>
+    </div>
+  );
+};
+
+export default Cart;
