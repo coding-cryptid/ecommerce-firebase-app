35c310b927982a848611dc432cfaea61a319cd1a
Gianna Cosenza <giacosey28@gmail.com>
2026-07-14 10:51:19 -0400

    Added store to the main

--- fakestore-app/src/main.tsx
+++ fakestore-app/src/main.tsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
