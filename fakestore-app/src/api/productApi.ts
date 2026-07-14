a56983d0c92ab0ba65fc0c4652b8809408839cc7
Gianna Cosenza <giacosey28@gmail.com>
2026-07-14 10:29:53 -0400

    Added fetching for category names and products

--- fakestore-app/src/api/productApi.ts
+++ fakestore-app/src/api/productApi.ts
@@ -5,3 +5,15 @@ export const fetchProducts = async (): Promise<Product[]> => {
   const response = await axios.get<Product[]>('https://fakestoreapi.com/products');
   return response.data;
 };
+
+export const fetchCategories = async (): Promise<string[]> => {
+  const response = await axios.get<string[]>('https://fakestoreapi.com/products/categories');
+  return response.data;
+};
+
+export const fetchProductsByCategory = async (category: string): Promise<Product[]> => {
+  const response = await axios.get<Product[]>(
+    `https://fakestoreapi.com/products/category/${category}`
+  );
+  return response.data;
+};
