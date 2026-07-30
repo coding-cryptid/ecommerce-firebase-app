432368bff9ff35d4d9ad79fba13533d0bf54be13
Gianna Cosenza <giacosey28@gmail.com>
2026-07-30 13:10:13 -0400

    uncommented

--- fakestore-app/src/api/productApi.ts
+++ fakestore-app/src/api/productApi.ts
@@ -1,19 +1,19 @@
-// import axios from 'axios';
-// import type { Product } from '../types/Product';
+import axios from 'axios';
+import type { Product } from '../types/Product';
 
-// export const fetchProducts = async (): Promise<Product[]> => {
-//   const response = await axios.get<Product[]>('https://fakestoreapi.com/products');
-//   return response.data;
-// };
+export const fetchProducts = async (): Promise<Product[]> => {
+  const response = await axios.get<Product[]>('https://fakestoreapi.com/products');
+  return response.data;
+};
 
-// export const fetchCategories = async (): Promise<string[]> => {
-//   const response = await axios.get<string[]>('https://fakestoreapi.com/products/categories');
-//   return response.data;
-// };
+export const fetchCategories = async (): Promise<string[]> => {
+  const response = await axios.get<string[]>('https://fakestoreapi.com/products/categories');
+  return response.data;
+};
 
-// export const fetchProductsByCategory = async (category: string): Promise<Product[]> => {
-//   const response = await axios.get<Product[]>(
-//     `https://fakestoreapi.com/products/category/${category}`
-//   );
-//   return response.data;
-// };
+export const fetchProductsByCategory = async (category: string): Promise<Product[]> => {
+  const response = await axios.get<Product[]>(
+    `https://fakestoreapi.com/products/category/${category}`
+  );
+  return response.data;
+};
